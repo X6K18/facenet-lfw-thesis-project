443cabe2b2ac5189cdf4c0f51a8bc0e81f1e6ed1
X6K18 <pn629503@gmail.com>
2026-04-26 21:09:01 +0700

    app

---
+++ app/config/paths.py
@@ -0,0 +1,8 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = ...\facenet\app
+
+ASSETS_DIR = os.path.join(BASE_DIR, "assets")
+ICON_DIR = os.path.join(ASSETS_DIR, "icons")
+IMAGE_DIR = os.path.join(ASSETS_DIR, "images")
